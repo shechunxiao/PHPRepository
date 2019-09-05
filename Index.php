39872a36d4b3cd50901a766f27868e55f66256a7
scx <282074579@qq.com>
2019-09-05 16:16:29 +0800

    magic注释

--- Index.php
+++ Index.php
@@ -12,20 +12,20 @@ require 'bootstrap/app.php';
 /**
  * magic魔术方法
  */
-$magic = new Magic();
-echo $magic->name;
-$magic->selfVar = 'selfVar';
-$magic->showSelfVar();
-if (isset($magic->selfVar)){
-    echo '能调用';
-}else{
-    echo '不能调用';
-}
-unset($magic->selfVar);
-echo $magic;
-
-$magic2 = clone $magic;
-echo $magic2->name;
+//$magic = new Magic();
+//echo $magic->name;
+//$magic->selfVar = 'selfVar';
+//$magic->showSelfVar();
+//if (isset($magic->selfVar)){
+//    echo '能调用';
+//}else{
+//    echo '不能调用';
+//}
+//unset($magic->selfVar);
+//echo $magic;
+//
+//$magic2 = clone $magic;
+//echo $magic2->name;
 
 
 
