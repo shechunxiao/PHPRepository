f0c7d8a2128a9b92d4777d3166c565b855c84575
scx <282074579@qq.com>
2020-12-19 16:04:46 +0800

    排序和单列

--- app/manual/NewArray.php
+++ app/manual/NewArray.php
@@ -23,6 +23,9 @@ class NewArray
 //        $this->frag_fit();
 //        $this->push_pop();
 //        $this->shift_unshift();
+//        $this->call_fun();
+//        $this->sort_fun();
+        $this->column_fun();
 
     }
 
@@ -526,6 +529,430 @@ class NewArray
 //        print_r($result); // 结果 4
 //        print_r($shift_array); // 结果 Array ( [0] => 5 [1] => 6 )
     }
-    
+
+    /**
+     * 回调函数(循环处理的函数)
+     * @note 回调函数的调用方式，array_walk($this->crr, [$this,'方法名'],['one'=>2,'two'=>4]);
+     * array_walk — 使用用户自定义函数对数组中的每个元素做回调处理
+     * array_map — 将回调函数作用到给定数组的单元上
+     * array_filter — 用回调函数过滤数组中的单元
+     * array_reduce — 用回调函数迭代地将数组简化为单一的值
+     */
+    public function call_fun()
+    {
+        /**
+         * @defination array_walk — 使用用户自定义函数对数组中的每个元素做回调处理
+         * @bool array_walk( array &$array, callable $funcname[, mixed $userdata = NULL] )
+         * @description 将用户自定义函数 funcname 应用到 array 数组中的每个单元。
+         * @notes array_walk() 不会受到 array 内部数组指针的影响。array_walk() 会遍历整个数组而不管指针的位置。
+         * @params array 输入的数组
+         * @params funcname 典型情况下 funcname 接受两个参数。array 参数的值作为第一个，键名作为第二个
+         *      Note:如果 funcname 需要直接作用于数组中的值，则给 funcname 的第一个参数指定为引用。这样任何对这些单元的改变也将会改变原始数组本身。
+         *      Note:只有 array 的值才可以被改变，用户不应在回调函数中改变该数组本身的结构。例如增加/删除单元，unset 单元等等。如果 array_walk() 作用的数组改变了，则此函数的的行为未经定义，且不可预期。
+         * @params userdata 如果提供了可选参数 userdata，将被作为第三个参数传递给 callback funcname。
+         * @return bool 成功时返回 TRUE， 或者在失败时返回 FALSE
+         */
+        $walk_array = [
+            ['name' => '张三', 'phone' => '123456789'],
+            ['name' => '李四', 'phone' => '123456789'],
+            ['name' => '王二', 'phone' => '123456789'],
+        ];
+        $param1 = '参数1';
+        $param2 = '参数2';
+
+        // 对每一个元素做回调处理，传值有两种方式，一种是使用use,另外一种是传递第三个参数，function中用第三个参数接收
+        // 如果想修改数组原来本身，则加&引用即可
+
+        // 方式一
+//        $result = array_walk($walk_array, function (&$value, $index, $params) {
+//
+//            $value['name'] = $params['param1'] . '-' . $value['name'] . '-' . $params['param2'];
+//
+//        }, ['param1' => $param1, 'param2' => $param2]);
+
+        // 方式二
+//        $params = ['param1' => $param1, 'param2' => $param2];
+//        $result = array_walk($walk_array, function (&$value, $index) use ($params) {
+//
+//            $value['name'] = $params['param1'] . '-' . $value['name'] . '-' . $params['param2'];
+//
+//        });
+//
+//        print_r($result); // 结果 1
+//        print_r($walk_array);
+
+        /**
+         * array_map — 将回调函数作用到给定数组的单元上
+         * array array_map( callable $callback, array $arr1[, array $...] )
+         * Note: map遍历的次数由后面数组中元素个数最大的决定
+         *      通常使用了两个或更多数组时，它们的长度应该相同，因为回调函数是平行作用于相应的单元上的。如果数组的长度不同，则最短的一个将被用空的单元扩充。
+         * array_map() 返回一个数组，该数组包含了 arr1 中的所有单元经过 callback 作用过之后的单元。callback 接受的参数数目应该和传递给 array_map() 函数的数组数目一致。
+         * @param callback 对每个数组的每个元素作用的回调函数
+         * @param arr1 将被回调函数（callback）执行的数组
+         * @param array 将被回调函数（callback）执行的数组列表
+         * @return array 返回一个数组，该数组的每个元素都数组（arr1）里面的每个元素经过回调函数（callback）处理了的
+         */
+
+        // 简单使用
+//        $a = [1, 2, 3, 4, 5];
+//        $result = array_map(function ($v) {
+//            return $v * $v * $v;
+//        }, $a);
+//        print_r($result); // 结果 Array ( [0] => 1 [1] => 8 [2] => 27 [3] => 64 [4] => 125 )
+
+        // 使用两个数组,返回的元素个数，由最大的数组元素决定
+        $a = [1, 2, 3, 4, 5];
+        $b = ['红', '橙', '黄', '绿', '青', '蓝', '紫色'];
+//        $result = array_map(function ($v1, $v2) {
+//            return $v1 . '-' . $v2;
+//        }, $a, $b);
+//        print_r($result); // 结果 Array ( [0] => 1-红 [1] => 2-橙 [2] => 3-黄 [3] => 4-绿 [4] => 5-青 [5] => -蓝 [6] => -紫色 )
+
+//        $result = array_map(function ($v1, $v2) {
+//            return [$v1 => $v2];
+//        }, $a, $b);
+//        print_r($result); // 结果 Array ( [0] => Array ( [1] => 红 ) [1] => Array ( [2] => 橙 ) [2] => Array ( [3] => 黄 ) [3] => Array ( [4] => 绿 ) [4] => Array ( [5] => 青 ) [5] => Array ( [] => 蓝 ) [6] => Array ( [] => 紫色 ) )
+
+        // 通过array_map实现构造数组，将多个数组合并为一个数组,回调函数为null
+//        $a = [1, 2, 3, 4, 5];
+//        $b = ['one', 'two', 'three', 'four', 'five'];
+//        $c = ['一', '二', '三', '四', '五'];
+//        $result = array_map(null, $a, $b, $c);
+//        print_r($result); // 结果 Array ( [0] => Array ( [0] => 1 [1] => one [2] => 一 ) [1] => Array ( [0] => 2 [1] => two [2] => 二 ) [2] => Array ( [0] => 3 [1] => three [2] => 三 ) [3] => Array ( [0] => 4 [1] => four [2] => 四 ) [4] => Array ( [0] => 5 [1] => five [2] => 五 ) )
+
+
+        // 如果数组参数里面有字符串的键，那么返回的数组就会包含字符串的键，仅且仅当只传入一个数组的时候（试了下，就是原数组返回，没变化啊，这不是蛋疼么？）。
+        // 如果不止一个数组被传入，那么返回的数组的的键都是整型。
+
+//        $a = ['one' => 1, 'two' => 2];
+//        $result = array_map(function ($v) {
+//            return array($v);
+//        }, $a);
+//        print_r($result); // 结果 Array ( [one] => Array ( [0] => 1 ) [two] => Array ( [0] => 2 ) )
+
+//        $a = ['one' => 1, 'two' => 2];
+//        $result = array_map(function ($v1, $v2) {
+//            return array($v1, $v2);
+//        }, $a, $a);
+//        print_r($result); // 结果 Array ( [0] => Array ( [0] => 1 [1] => 1 ) [1] => Array ( [0] => 2 [1] => 2 ) )
+
+        /**
+         * array_filter
+         * array_filter — 用回调函数过滤数组中的单元
+         * array array_filter( array $array[, callable $callback[, int $flag = 0]] )
+         * 依次将 input 数组中的每个值传递到 callback 函数。如果 callback 函数返回 TRUE，则 input 数组的当前值会被包含在返回的结果数组中。
+         * @param input 要循环的数组
+         * @param callback 使用的回调函数
+         *      Note: 如果没有提供 callback 函数，将删除 input 中所有等值为 FALSE 的条目。
+         *      当转换为 boolean 时，以下值被认为是 FALSE：
+         * ◦ 布尔值 FALSE 本身
+         * ◦ 整型值 0（零）
+         * ◦ 浮点型值 0.0（零）
+         * ◦ 空字符串，以及字符串 "0"
+         * ◦ 不包括任何元素的数组
+         * ◦ 不包括任何成员变量的对象（仅 PHP 4.0 适用）
+         * ◦ 特殊类型 NULL（包括尚未赋值的变量）
+         * ◦ 从空标记生成的 SimpleXML 对象
+         * 所有其它值都被认为是 TRUE（包括任何资源）。
+         * @param flag 决定callback接收的参数形式
+         * ◦ ARRAY_FILTER_USE_KEY - callback接受键名作为的唯一参数
+         * ◦ ARRAY_FILTER_USE_BOTH - callback同时接受键名和键值
+         * @return array 返回过滤后的数组
+         */
+        // 输出都为奇数的
+//        $filter_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//        $result = array_filter($filter_array,function($value,$index){
+//            return ($value & 1);
+//        },ARRAY_FILTER_USE_BOTH);
+//        print_r($result);
+
+        // 输出都为偶数的
+//        $filter_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//        $result = array_filter($filter_array,function($value,$index){
+//            return !($value & 1);
+//        },ARRAY_FILTER_USE_BOTH);
+//        print_r($result);
+
+        // 去除数组中为空的
+//        $filter_array = [1, 5, null, '', false, 0, 0.0, '0', '0.0'];
+//        $result = array_filter($filter_array);
+//        print_r($result);
+
+        /**
+         * array_reduce — 用回调函数迭代地将数组简化为单一的值
+         * mixed array_reduce( array $input, callable $function[, mixed $initial = NULL] )
+         * array_reduce() 将回调函数 function 迭代地作用到 input 数组中的每一个单元中，从而将数组简化为单一的值。
+         * @param input 输入的数组
+         * @param callable 回调函数
+         * @param initial 如果指定了可选参数 initial，该参数将被当成是数组中的第一个值来处理，或者如果数组为空的话就作为最终返回值
+         * @return mixed initial 参数，array_reduce() 返回 NULL。
+         */
+
+        // 如果指定了可选参数 initial，该参数将被当成是数组中的第一个值来处理，或者如果数组为空的话就作为最终返回值
+//        $reduce_array = [];
+//        $result = array_reduce($reduce_array, function () {
+//            return;
+//        }, '初始值');
+//        print_r($result); // 结果 初始值
+
+        // 指定initial,并且use使用参数
+//        $reduce_array = ['one', 'two'];
+//        $str = '/';
+//        $result = array_reduce($reduce_array, function ($v1, $v2) use ($str) {
+//            return $v1 . $v2 . $str;
+//        }, '@');
+//        print_r($result); // 结果 @one/two/
+
+        // 上面都是处理一维数组，下面看一下二维数组
+//        $reduce_array = [
+//            ['name' => '张三'],
+//            ['name' => '李四'],
+//        ];
+//        $result = array_reduce($reduce_array, function ($v1, $v2) {
+//            $temp = [];
+//            $temp['name'] = $v1['name'] . '/' . $v2['name'];
+//            return $temp;
+//        }, ['name' => '第一个初始name']);
+//        print_r($result); // 结果 Array ( [name] => /张三/李四 )
+
+        // 复杂点的应用,组合方式
+//        $arr = [
+//            ['红', '黄', '蓝'],
+//            ['大', '中', '小']
+//        ];
+//        $str = ';';
+//        $result = array_reduce($arr, function ($v, $m) use ($str) {
+//            $temp = [];
+//            foreach ($v as $vv) {
+//                foreach ($m as $mm) {
+//                    if (empty($vv)) {
+//                        $temp[] = $mm;
+//                    } else {
+//                        $temp[] = $vv . $str . $mm;
+//                    }
+//                }
+//            }
+//            return $temp;
+//        }, ['']);
+//        print_r($result); // 结果 Array ( [0] => 红;大 [1] => 红;中 [2] => 红;小 [3] => 黄;大 [4] => 黄;中 [5] => 黄;小 [6] => 蓝;大 [7] => 蓝;中 [8] => 蓝;小 )
+
+    }
+
+    /**
+     * 排序
+     * 无r:正向排序，有r:逆向排序
+     * 无a:排序后的索引从0开始，有a:保持原来的索引关系
+     * sort — 对数组升序排序
+     * rsort — 对数组逆向排序(用法与sort一致)
+     * asort — 对数组进行排序并保持索引关系
+     * arsort — 对数组进行逆向排序并保持索引关系
+     * ksort — 对数组按照键名排序
+     * krsort — 对数组按照键名逆向排序
+     * 特殊排序
+     * natsort — 用"自然排序"算法对数组排序
+     * natcasesort — 用"自然排序"算法对数组进行不区分大小写字母的排序
+     */
+    public function sort_fun()
+    {
+        /**
+         * sort — 对数组升序排序
+         * bool sort( array &$array[, int $sort_flags = SORT_REGULAR] )
+         * 本函数对数组进行排序。当本函数结束时数组单元将被从最低到最高重新安排。
+         *      Note: 此函数为 array 中的元素赋与新的键名。这将删除原有的键名，而不是仅仅将键名重新排序。
+         * @param array 要排序的数组
+         * @param sort_flags 可选的第二个参数 sort_flags 可以用以下值改变排序的行为：
+         * 排序类型标记：
+         * ◦ SORT_REGULAR - 正常比较单元（不改变类型） 把每一项按常规顺序排列（Standard ASCII，不改变类型）
+         * ◦ SORT_NUMERIC - 单元被作为数字来比较
+         * ◦ SORT_STRING - 单元被作为字符串来比较
+         * ◦ SORT_LOCALE_STRING - 根据当前的区域（locale）设置来把单元当作字符串比较，可以用 setlocale() 来改变。
+         * ◦ SORT_NATURAL - 和 natsort() 类似对每个单元以"自然的顺序"对字符串进行排序。 PHP 5.4.0 中新增的。
+         * ◦ SORT_FLAG_CASE - 能够与 SORT_STRING 或 SORT_NATURAL 联合使用（OR 位运算），不区分大小写排序字符串。
+         * @return bool 成功时返回 TRUE， 或者在失败时返回 FALSE
+         */
+        // SORT_REGULAR 是按照标准ASCII码排序的，不改变类型，注意这里都是字符，比如'1','2'
+        // SORT_NUMERIC 是按照数字来比较
+        // SORT_STRING  是按照字符串来比较
+        // SORT_LOCALE_STRING 根据当前的区域（locale）设置来把单元当作字符串比较(基本不用)
+        // SORT_NATURAL 对每个单元以"自然的顺序"对字符串进行排序,可以理解为，先根据首字母ASCII排序，然后第二个，第三个，依次类推,如果遇到数字，将会把数字部分整体排序，而不是一个个
+        // SORT_FLAG_CASE 与 SORT_STRING 或 SORT_NATURAL 联合使用,不区分大小写排序字符串
+
+
+        // SORT_REGULAR 默认的正常排序-纯数字
+//        $a = [12, 34, 56, 29, 90, 54];
+//        $result = sort($a);
+//        print_r($a); // 结果 Array ( [0] => 12 [1] => 29 [2] => 34 [3] => 54 [4] => 56 [5] => 90 )
+
+        // SORT_REGULAR 默认的正常排序-纯字母/字符
+//        $a = ['b', 'a', '/', 'z', 'H', 'A', 'B', '\\', '<', '_', '0', '1', '2'];
+//        $result = sort($a);
+//        print_r($a); // 结果 Array ( [0] => / [1] => A [2] => B [3] => H [4] => \ [5] => a [6] => b [7] => z )
+
+
+        // SORT_REGULAR 默认的正常排序-字符+中文
+//        $a = ['无敌', '字符', 'a', 'ab', 'b', '30', '301', '张三', '李四', '王二', '麻子'];
+//        $result = sort($a);
+//        print_r($a); // 结果 Array ( [0] => 30 [1] => 301 [2] => a [3] => ab [4] => b [5] => 字符 [6] => 张三 [7] => 无敌 [8] => 李四 [9] => 王二 [10] => 麻子 )
+
+        // SORT_REGULAR 默认的正常排序-中文
+//        $a = ['测试', '什么', '情况', '可以', '很好', '非常牛'];
+//        $result = sort($a); // 结果 Array ( [0] => 什么 [1] => 可以 [2] => 很好 [3] => 情况 [4] => 测试 [5] => 非常牛 )
+//        print_r($a);
+
+        // SORT_NUMERIC 按照数字来比较-数字/数字字符
+//        $a = [1, '1', 2, '21', 89, '76', true, false, null];
+//        $result = sort($a, SORT_NUMERIC);
+//        print_r($a); // 结果 Array ( [0] => [1] => [2] => 1 [3] => 1 [4] => 1 [5] => 2 [6] => 21 [7] => 76 [8] => 89 )
+
+        // SORT_STRING 按照字符串来比较-字符串
+//        $a = ['1', 10, '99', '76', '102', 38, '45'];
+//        $result = sort($a,SORT_STRING);
+//        print_r($a); // 结果 Array ( [0] => 1 [1] => 10 [2] => 102 [3] => 38 [4] => 45 [5] => 76 [6] => 99 )
+
+        // SORT_LOCALE_STRING 根据当前的区域（locale）设置来把单元当作字符串比较，可以用 setlocale() 来改变
+//        $a = ['20', '201', 1, '5', 6];
+//        $result = sort($a, SORT_LOCALE_STRING);
+//        print_r($a); // 结果 Array ( [0] => 1 [1] => 20 [2] => 201 [3] => 5 [4] => 6 )
+
+        // SORT_NATURAL 对每个单元以"自然的顺序"对字符串进行排序
+//        $a = ["Orange1", "orange2", "Orange3", "orange20"];
+//        $result = sort($a, SORT_NATURAL);
+//        print_r($a); // 结果 Array ( [0] => Orange1 [1] => Orange3 [2] => orange2 [3] => orange20 )
+
+        // SORT_FLAG_CASE 与 SORT_STRING 或 SORT_NATURAL 联合使用,不区分大小写排序字符串
+//        $a = ["Orange1d", "orange2c", "Orange3b", "orange20a"];
+//        $result = sort($a, SORT_NATURAL | SORT_FLAG_CASE);
+//        print_r($a); // 结果 Array ( [0] => Orange1d [1] => orange2c [2] => Orange3b [3] => orange20a )
+
+        /**
+         * rsort — 对数组逆向排序(用法与sort一致)
+         * bool rsort( array &$array[, int $sort_flags = SORT_REGULAR] )
+         * @param input 输入的数组
+         * @param sort_flogs 可以用可选参数 sort_flags 改变排序的行为，参数与sort相同
+         * @return bool 成功时返回 TRUE， 或者在失败时返回 FALSE
+         */
+//        $a = [12, 34, 56, 29, 90, 54];
+//        $result = rsort($a);
+//        print_r($a); // 结果 Array ( [0] => 90 [1] => 56 [2] => 54 [3] => 34 [4] => 29 [5] => 12 )
+
+        /**
+         * asort — 对数组进行排序并保持索引关系
+         * bool asort( array &$array[, int $sort_flags = SORT_REGULAR] )
+         * 本函数对数组进行排序，数组的索引保持和单元的关联。主要用于对那些单元顺序很重要的结合数组进行排序。
+         * @param input 输入的数组
+         * @param sort_flogs 可以用可选参数 sort_flags 改变排序的行为，参数与sort相同
+         * @return bool 成功时返回 TRUE， 或者在失败时返回 FALSE
+         */
+//        $a = [12, 34, 56, 29, 90, 54];
+//        $result = asort($a);
+//        print_r($a); // 结果 Array ( [0] => 12 [3] => 29 [1] => 34 [5] => 54 [2] => 56 [4] => 90 )
+
+        /**
+         * arsort — 对数组进行逆向排序并保持索引关系
+         * bool arsort( array &$array[, int $sort_flags = SORT_REGULAR] )
+         * 本函数对数组进行排序，数组的索引保持和单元的关联 主要用于对那些单元顺序很重要的结合数组进行排序。
+         * @param input 输入的数组
+         * @param sort_flogs 可以用可选参数 sort_flags 改变排序的行为，参数与sort相同
+         * @return bool 成功时返回 TRUE， 或者在失败时返回 FALSE
+         */
+//        $a = [12, 34, 56, 29, 90, 54];
+//        $result = arsort($a);
+//        print_r($a); // 结果 Array ( [4] => 90 [2] => 56 [5] => 54 [1] => 34 [3] => 29 [0] => 12 )
+
+        /**
+         * ksort — 对数组按照键名排序
+         * bool ksort( array &$array[, int $sort_flags = SORT_REGULAR] )
+         * 对数组按照键名排序，保留键名到数据的关联。本函数主要用于关联数组。
+         * @param input 输入的数组
+         * @param sort_flogs 可以用可选参数 sort_flags 改变排序的行为，参数与sort相同
+         * @return bool 成功时返回 TRUE， 或者在失败时返回 FALSE
+         */
+//        $a = [4=>12, 2=>34, 6=>56, 10=>29, 1=>90, 3=>54];
+//        $result = ksort($a);
+//        print_r($a); // 结果 Array ( [1] => 90 [2] => 34 [3] => 54 [4] => 12 [6] => 56 [10] => 29 )
+
+        /**
+         * krsort — 对数组按照键名逆向排序
+         * bool krsort( array &$array[, int $sort_flags = SORT_REGULAR] )
+         * 对数组按照键名逆向排序，保留键名到数据的关联。主要用于结合数组。
+         * @param input 输入的数组
+         * @param sort_flogs 可以用可选参数 sort_flags 改变排序的行为，参数与sort相同
+         * @return bool 成功时返回 TRUE， 或者在失败时返回 FALSE
+         */
+//        $a = [4=>12, 2=>34, 6=>56, 10=>29, 1=>90, 3=>54];
+//        $result = krsort($a);
+//        print_r($a); // 结果 Array ( [10] => 29 [6] => 56 [4] => 12 [3] => 54 [2] => 34 [1] => 90 )
+
+        /**
+         * natsort — 用"自然排序"算法对数组排序
+         * natsort与asort的SORT_NATURAL有异曲同工之妙
+         * bool natsort( array &$array)
+         * 函数实现了一个和人们通常对字母数字字符串进行排序的方法一样的排序算法并保持原有键／值的关联，这被称为"自然排序"。
+         * @param array  输入的 array
+         * @return 成功时返回 TRUE， 或者在失败时返回 FALSE
+         */
+//        $a = ["Orange1", "orange2", "Orange3", "orange20"];
+//        $result = natsort($a);
+//        print_r($a); // 结果 Array ( [0] => Orange1 [2] => Orange3 [1] => orange2 [3] => orange20 )
+
+        /**
+         * natcasesort — 用"自然排序"算法对数组进行不区分大小写字母的排序
+         * bool natcasesort( array &$array)
+         * natcasesort() 是 natsort() 函数的不区分大小写字母的版本
+         * 本函数实现了一个和人们通常对字母数字字符串进行排序的方法一样的排序算法并保持原有键／值的关联，这被称为"自然排序"。
+         * @param array  输入的 array
+         * @return 成功时返回 TRUE， 或者在失败时返回 FALSE
+         */
+//        $a = ["Orange1", "orange2", "Orange3", "orange20"];
+//        $result = natcasesort($a);
+//        print_r($a); // 结果 Array ( [0] => Orange1 [1] => orange2 [2] => Orange3 [3] => orange20 )
+
+    }
+
+    /**
+     * 列
+     * array_column — 返回数组中指定的一列
+     */
+    public function column_fun()
+    {
+        /**
+         * array_column — 返回数组中指定的一列
+         * array array_column( array $input, mixed $column_key[, mixed $index_key] )
+         * array_column() 返回input数组中键值为column_key的列，如果指定了可选参数index_key，
+         * 那么input数组中的这一列的值将作为返回数组中对应值的键。
+         * @param input 需要取出数组列的多维数组（或结果集）
+         * @param column_key 需要返回值的列，它可以是索引数组的列索引，或者是关联数组的列的键。
+         * 也可以是NULL，此时将返回整个数组（配合index_key参数来重置数组键的时候，非常管用）
+         * @param index_key 作为返回数组的索引/键的列，它可以是该列的整数索引，或者字符串键值
+         * @return 从多维数组中返回单列数组(只能是单列)
+         */
+        $a = [
+            [
+                'id' => 1,
+                'name' => 'first'
+            ],
+            [
+                'id' => 2,
+                'name' => 'second'
+            ]
+        ];
+        // 不指定列，不指定$index_key,就是原数组
+//        $result = array_column($a,null);
+//        print_r($result);
+
+        // 指定列,不指定$index_key
+//        $result = array_column($a, 'name');
+//        print_r($result); // 结果 Array ( [0] => first [1] => second )
+
+        // 指定列，指定$index_key，如果$index_key不存在,则为索引数组
+//        $result = array_column($a, 'name', 'id');
+//        print_r($result); // 结果 Array ( [1] => first [2] => second )
+
+        // 不指定列，指定$index_key
+//        $result = array_column($a, null, 'id');
+//        print_r($result); // 结果 Array ( [1] => Array ( [id] => 1 [name] => first ) [2] => Array ( [id] => 2 [name] => second ) )
+
+    }
+
 
 }
