44c49f458932a71dd8cd445df74b47fc2104a576
scx <282074579@qq.com>
2020-12-28 17:48:44 +0800

    字符串

--- app/manual/NewString.php
+++ app/manual/NewString.php
@@ -11,6 +11,40 @@ class NewString
 {
     public function index()
     {
-
+        /**
+         * addcslashes
+         */
+//        $str = addcslashes('reset', 'r');
+//        echo $str.'</br>';
+//        $str = stripcslashes($str);
+//        echo $str.'</br>';
+//        $str = addslashes("Is \your name  O'reilly?");
+//        echo $str.'</br>';
+//        $str = stripslashes($str);
+//        echo $str.'</br>';
+//        $str = chunk_split('ceshisjfdkskf',2);
+//        echo $str.'</br>'; // ce sh is jf dk sk f
+//        $str = str_split('ceshisjfdkskf',2);
+//        print_r($str); // Array ( [0] => ce [1] => sh [2] => is [3] => jf [4] => dk [5] => sk [6] => f )
+//        $str = convert_cyr_string('中华人民共和国','k-koi8-r','a-x-cp866'); // 弃用了
+//        echo $str;
+//        $str = convert_uuencode('疯狂夺金是复健科了单身快乐发多少');
+//        echo $str.'</br>';
+//        $str = convert_uudecode('疯狂夺金是复健科了单身快乐发多少');
+//        echo $str.'</br>';
+//        $str = count_chars("testiste",0);
+//        print_r($str);
+//        $str = crc32("The quick brown fox jumped over the lazy dog.");
+//        echo $str.'</br>';
+//        $str = crypt('password','233');
+//        echo $str.'</br>';
+//        $str = htmlentities('<div>张三</div>');
+//        echo $str.'</br>';
+//        $str = '<p>Test paragraph.</p><!-- Comment --> <a href="#fragment">Other text</a>';
+//        echo strip_tags($str).'</br>';
+//        $str = levenshtein( '放假圣诞节快乐发大水口', '放塑料袋口放款了减多少');
+//        echo strip_tags($str).'</br>';
+//        $str = chop('sfds');
+//        echo strip_tags($str).'</br>';
     }
 }
