0485e24b78511c1b9f76fa4646f60c9cd6918c48
scx <282074579@qq.com>
2021-01-08 09:00:50 +0800

    字符串

--- app/manual/NewDate.php
+++ app/manual/NewDate.php
@@ -14,7 +14,25 @@ class NewDate
      */
     public function index()
     {
-        echo time();
+//        echo time();
+//        $getdate = getdate();
+//        print_r($getdate);
+//        echo date('Y').'</br>';
+//        echo date('m').'</br>';
+//        echo date('d').'</br>';
+//        echo date('H').'</br>';
+//        echo date('i').'</br>';
+//        echo date('s').'</br>';
+//        echo date('l').'</br>';
+//        echo date('N').'</br>';
+//        echo date('S').'</br>';
+//        echo date('w').'</br>';
+//        echo date('z',strtotime('2021-12-31 13:56:23')).'</br>';
+//        echo date('L',strtotime('2024-05-12')).'</br>';
+//        echo date('O').'</br>';
+//        echo date('U').'</br>';
+//        echo microtime().'</br>';
+//        echo microtime(true).'</br>';
     }
 
 
