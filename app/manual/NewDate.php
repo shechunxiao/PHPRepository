8bc5e5de95fdc6a219aefaab5c22048c7870a7f1
282074579@qq.com <282074579@qq.com>
2021-01-10 11:23:22 +0800

    123

--- app/manual/NewDate.php
+++ app/manual/NewDate.php
@@ -42,53 +42,67 @@ class NewDate
 //        $result = checkdate('12','32','2020'); // 错误值
 //        var_dump($result);
 
-        $time = strtotime('2021-01-09 17:44:56'); // 时间戳 1610185496
+//        $time = strtotime('2021-01-09 17:44:56'); // 时间戳 1610185496
+//
+//        echo '***************日***************'.'</br>';
+//        echo date('d',$time).'</br>'; // 09
+//        echo date('D',$time).'</br>'; // Sat
+//        echo date('j',$time).'</br>'; // 9
+//        echo date('l',$time).'</br>'; // Saturday
+//        echo date('N',$time).'</br>'; // 6
+//        echo date('S',$time).'</br>'; // th
+//        echo date('w',$time).'</br>'; // 6
+//        echo date('z',$time).'</br>'; // 8
+//        echo '***************星期***************'.'</br>';
+//        echo date('W',$time).'</br>'; // 01
+//        echo '***************月***************'.'</br>';
+//        echo date('F',$time).'</br>'; // January
+//        echo date('m',$time).'</br>'; // 01
+//        echo date('M',$time).'</br>'; // Jan
+//        echo date('n',$time).'</br>'; // 1
+//        echo date('t',$time).'</br>'; // 31
+//        echo '***************年***************'.'</br>';
+//        echo date('L',$time).'</br>'; // 0
+//        echo date('o',$time).'</br>'; // 2021
+//        echo date('Y',$time).'</br>'; // 2021
+//        echo date('y',$time).'</br>'; // 21
+//        echo '***************时间***************'.'</br>';
+//        echo date('a',$time).'</br>'; // pm
+//        echo date('A',$time).'</br>'; // PM
+//        echo date('g',$time).'</br>'; // 5
+//        echo date('G',$time).'</br>'; // 17
+//        echo date('h',$time).'</br>'; // 05
+//        echo date('H',$time).'</br>'; // 17
+//        echo date('i',$time).'</br>'; // 44
+//        echo date('s',$time).'</br>'; // 56
+//        echo date('u',$time).'</br>'; // 000000
+//        echo '***************时区***************'.'</br>';
+//        echo date('e',$time).'</br>'; // Asia/Shanghai
+//        echo date('I',$time).'</br>'; // 0
+//        echo date('O',$time).'</br>'; // +0800
+//        echo date('P',$time).'</br>'; // +08:00
+//        echo date('T',$time).'</br>'; // CST
+//        echo date('Z',$time).'</br>'; // 28800
+//        echo date('c',$time).'</br>'; // 2021-01-09T17:44:56+08:00
+//        echo date('r',$time).'</br>'; // Sat, 09 Jan 2021 17:44:56 +0800
+//        echo date('U',$time).'</br>'; // 1610185496
+//        echo '***************其他***************'.'</br>';
+//        echo date(DATE_RFC3339).'</br>'; // 2021-01-10T10:12:36+08:00
+//        echo date(DATE_ISO8601).'</br>'; // 2021-01-10T10:12:36+0800
 
-        echo '***************日***************'.'</br>';
-        echo date('d',$time).'</br>';
-        echo date('D',$time).'</br>';
-        echo date('j',$time).'</br>';
-        echo date('l',$time).'</br>';
-        echo date('N',$time).'</br>';
-        echo date('S',$time).'</br>';
-        echo date('w',$time).'</br>';
-        echo date('z',$time).'</br>';
-        echo '***************星期***************'.'</br>';
-        echo date('W',$time).'</br>';
-        echo '***************月***************'.'</br>';
-        echo date('F',$time).'</br>';
-        echo date('m',$time).'</br>';
-        echo date('M',$time).'</br>';
-        echo date('n',$time).'</br>';
-        echo date('t',$time).'</br>';
-        echo '***************年***************'.'</br>';
-        echo date('L',$time).'</br>';
-        echo date('o',$time).'</br>';
-        echo date('Y',$time).'</br>';
-        echo date('y',$time).'</br>';
-        echo '***************时间***************'.'</br>';
-        echo date('a',$time).'</br>';
-        echo date('A',$time).'</br>';
-        echo date('g',$time).'</br>';
-        echo date('G',$time).'</br>';
-        echo date('h',$time).'</br>';
-        echo date('H',$time).'</br>';
-        echo date('i',$time).'</br>';
-        echo date('s',$time).'</br>';
-        echo date('u',$time).'</br>';
-        echo '***************时区***************'.'</br>';
-        echo date('e',$time).'</br>';
-        echo date('I',$time).'</br>';
-        echo date('O',$time).'</br>';
-        echo date('P',$time).'</br>';
-        echo date('T',$time).'</br>';
-        echo date('Z',$time).'</br>';
-        echo date('c',$time).'</br>';
-        echo date('r',$time).'</br>';
-        echo date('U',$time).'</br>';
-        echo '***************其他***************'.'</br>';
-        echo date(DATE_RFC3339).'</br>';
-        echo date(DATE_ISO8601).'</br>';
+//        echo time();
+
+//        echo microtime().'</br>';
+//        echo microtime(true).'</br>';
+//        echo strtotime('2021-01-10 10:52:53'); // 1610247173
+//        echo strtotime('2021-01-10 10:52:59','1610247173');
+
+//        $result = getdate();
+//        print_r($result);
+
+//        $result = date_parse('2021-01-10 11:05:56');
+//        print_r($result);
+
     }
 
 
